66f7d5dc36bdce1a0ba5ea298798a5036c3b7999
canace-wang1 <ping.wang@verycloud.cn>
2026-05-19 16:58:28 +0800

    fix: 改为静态模式部署

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,5 @@
 import { defineConfig } from 'astro/config';
-import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
-  output: 'server', // 开启 SSR，方便后面对接 Workers API
-  adapter: cloudflare(),
+  output: 'static',
 });
